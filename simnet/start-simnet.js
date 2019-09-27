b9fc7369646fd0ad73c9ad92177c8a2008d03acd
Buck Perley <bucko.perley@gmail.com>
2019-09-26 20:08:15 -0500

    add funding for bob and carol

--- simnet/start-simnet.js
+++ simnet/start-simnet.js
@@ -11,6 +11,11 @@ const env = {
   COMPOSE_INTERACTIVE_NO_CLI: true,
 }
 
+function mineBlocks(num=1) {
+  assert(typeof num === 'number')
+  return exec(`docker-compose run btcctl generate 400`, { env }) 
+}
+
 /**
  * A class used to create NodeConfigs for creating and interacting simnet
  * lightning nodes via docker-compose
@@ -67,15 +72,17 @@ class NodeConfig {
     while (!connection && counter < 10) {
       try {
         const info = await this.getInfo()
-        if (info && info.version) connection = true
+        if (info && info.version) {
+          this.setIdentity(info.identity_pubkey)
+          connection = true
+        }
         counter++
       } catch (e) {}
       counter++
     }
     if (!connection) throw new Error('Could not establish connection with node')
 
-    await this.setIdentity()
-    console.log(`${this.name.toUpperCase()}: ${this.identityPubkey}`)
+    console.log(`${this.name.toUpperCase()} pubkey: ${this.identityPubkey}`)
     return
   }
 
@@ -86,15 +93,8 @@ class NodeConfig {
     return exec(`${this.lncli} ${cmd}`, { env: this.env })
   }
 
-  async setIdentity() {
-    const resp = (await this.exec('getinfo'))
-    if (resp.stderr) throw new Error(stderr)
-    try {
-      this.identityPubkey = JSON.parse(resp.stdout).identity_pubkey
-    } catch (e) {
-      console.log('Coudnt parse stdout:', resp.stdout)
-      throw e
-    }
+  async setIdentity(pubkey) {
+    this.identityPubkey = pubkey
   }
 
   async getInfo() {
@@ -102,6 +102,28 @@ class NodeConfig {
 
     if (resp.stderr.length) console.error('Problem connecting to node:', resp.stderr)
     return JSON.parse(resp.stdout)
+  }
+
+  async getAddress() {
+    try {
+      let { stdout, stderr} = await this.exec('newaddress np2wkh')
+      if (stdout && stdout.length)
+        return JSON.parse(stdout).address
+      else if (stderr) {
+        console.error(stderr)
+      }
+    } catch (e) {}
+  }
+
+  async getBalance() {
+    try {
+      let { stdout, stderr} = await this.exec('walletbalance')
+      if (stdout && stdout.length)
+        return JSON.parse(stdout)
+      else if (stderr) {
+        console.error(stderr)
+      }
+    } catch (e) {}
   }
 }
 
@@ -115,19 +137,10 @@ class NodeConfig {
   // Get an address from alice's node to use as the mining address for our full node
   // Needs to be a loop because sometimes even if the container is started
   // the node process may not have fully booted yet
+  console.log(`Getting address from alice for mining reward destination.`)
   let MINING_ADDRESS, counter = 1, tries = 15
   while (!MINING_ADDRESS && counter < tries) {
-    try {
-      console.log(`Getting address from alice node.`)
-      let { stdout, stderr} = await alice.exec('newaddress np2wkh')
-      
-      if (stdout)
-        MINING_ADDRESS = JSON.parse(stdout).address
-      else if (stderr) {
-        console.error(stderr)
-        break
-      }
-    } catch (e) {}
+    MINING_ADDRESS = await alice.getAddress()
     counter++
   }
 
@@ -146,8 +159,7 @@ class NodeConfig {
     let blockchainInfo = (await exec(`docker-compose run btcctl getblockchaininfo`)).stdout
     blockchainInfo = JSON.parse(blockchainInfo)
 
-    let balance = (await alice.exec('walletbalance')).stdout
-    balance = JSON.parse(balance)
+    let balance = await alice.getBalance()
 
     // check if we already have a mined blockchain and funded wallet on persisted volume
     if (
@@ -162,19 +174,49 @@ class NodeConfig {
     } else {
       console.log('No existing simnet chain found. Creating new one.')
       console.log('Mining 400 blocks...')
-      await exec(`docker-compose run btcctl generate 400`, { env }) 
+      await mineBlocks(400)
       let balance = (await alice.exec('walletbalance')).stdout
       balance = JSON.parse(balance)
       console.log(`Alice's balance: ${balance.confirmed_balance}`)
     }
 
-    // Next let's startup nodes for bob and carol using a neutrino backend
+    // Startup nodes for bob and carol using a neutrino backend
     const bob = new NodeConfig({ name: 'bob', port: 10002, neutrino: true })
     await bob.startNode()
 
     const carol = new NodeConfig({ name: 'carol', port: 10003, neutrino: true })
     await carol.startNode()
+
+    // Fund bob and carol from alice's wallet
+    console.log('Funding bob and carol...')
+    const bobAddr = await bob.getAddress()
+    const carolAddr = await carol.getAddress()
+
+    // send from alice to bob
+    let aliceBalance = await alice.getBalance()
+    let bobBalance = await bob.getBalance()
+    let carolBalance = await carol.getBalance()
+    
+    const amount = Math.floor(aliceBalance.confirmed_balance / 4)
+
+    console.log(`Sending ${amount} satoshis to bob and carol.`)
+    
+    await alice.exec(`sendcoins ${bobAddr} ${amount}`)
+    // send from alice to carol
+    await alice.exec(`sendcoins ${carolAddr} ${amount}`)
+
+    await mineBlocks()
+
+    aliceBalance = await alice.getBalance()
+    bobBalance = await bob.getBalance()
+    carolBalance = await carol.getBalance()
+
+    console.log('New Balances')
+    console.log('Alice: ', aliceBalance.confirmed_balance)
+    console.log('Bob: ', bobBalance.confirmed_balance)
+    console.log('Carol: ', carolBalance.confirmed_balance)
   } catch (e) {
-    console.error('problem starting node:', e)
+    if (e.stderr) console.error('Encountered error starting network:', e.stderr)
+    else console.error('Encountered error:', e)
   }
 })()
